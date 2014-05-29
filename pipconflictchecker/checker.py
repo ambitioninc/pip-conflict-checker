def30f0fb1e3cfc0c3a35f1ec976caf2b1499891
Ohhhh Yeahhhhhhhh <jared.lewis@ambition.com>
2014-05-29 09:55:23 -0400

    * Updates to account for multiple version ranges, exacts, and excludes

--- pipconflictchecker/checker.py
+++ pipconflictchecker/checker.py
@@ -1,18 +1,196 @@
 import pip
 from pkg_resources import parse_version
+from pprint import pprint
 
 
 class Conflict(object):
     """
     Class that contains information about a dependency conflict
     """
-    def __init__(self, project_name, required_project_name, comparison, expected_version, installed_version):
+    def __init__(self, project_name, required_project_name, installed_version, specs):
         super(Conflict, self).__init__()
         self.project_name = project_name
         self.required_project_name = required_project_name
-        self.comparison = comparison
-        self.expected_version = expected_version
         self.installed_version = installed_version
+        self.specs = specs
+        self.readable_specs = self.create_readable_specs()
+
+    def create_readable_specs(self):
+        readable_specs = []
+        for spec in self.specs:
+            readable_specs.append('{0}{1}'.format(
+                spec[0],
+                spec[1]
+            ))
+        return ','.join(readable_specs)
+
+
+class Validator(object):
+    def __init__(self, installed_version, required_version_specs):
+        super(Validator, self).__init__()
+        self.installed_version = installed_version
+        self.required_version_specs = sorted(required_version_specs, key=lambda spec: parse_version(spec[1]))
+
+    def is_valid(self):
+        """
+        Checks that the installed version is valid within the required versions
+        """
+        # Init is valid to false
+        is_valid = False
+
+        # Get the booleans of all the checks
+        in_ranges = self.in_ranges()
+        in_exacts = self.in_exacts()
+        in_excludes = self.in_excludes()
+
+        # Determine if this is a valid installed version
+        if (in_ranges or in_exacts) and not in_excludes:
+            is_valid = True
+
+        return is_valid
+
+    def in_ranges(self):
+        """
+        Determine if the installed version is in one of the required ranges
+        """
+        # Get the ranges
+        ranges = self.get_required_version_ranges()
+
+        # Set the default to false
+        in_ranges = False
+
+        # Keep a list of the results for determining if a version is within a range
+        results = []
+
+        # Loop over the ranges and determine if the installed version is in this range
+        for spec_range in ranges:
+            spec_results = []
+            for spec in spec_range:
+                if spec is not None:
+                    conditional = 'parse_version(self.installed_version) {0} parse_version(spec[1])'.format(
+                        spec[0]
+                    )
+                    spec_results.append(eval(conditional))
+
+            # If any spec was false the overall range is false
+            if False in spec_results:
+                results.append(False)
+            else:
+                results.append(True)
+
+        # If the installed version is within any of the ranges, the overall result is true
+        if True in results:
+            in_ranges = True
+
+        # Return the result
+        return in_ranges
+
+    def in_exacts(self):
+        """
+        Determine if the installed version matches one of the exact versions
+        """
+        # Set the default response to false
+        in_exacts = False
+
+        # Loop over the specs and check for an exact match
+        exacts = self.get_required_version_exacts()
+        for spec in exacts:
+            if spec[1] == self.installed_version:
+                in_exacts = True
+
+        # Return the response
+        return in_exacts
+
+    def in_excludes(self):
+        """
+        Determine if the installed version matches one of the excluded versions
+        """
+        # Set the default response to false
+        in_excludes = False
+
+        # Check installed version against the excluded versions
+        excludes = self.get_required_version_excludes()
+        for spec in excludes:
+            if spec[1] == self.installed_version:
+                in_excludes = True
+
+        # Return the response
+        return in_excludes
+
+    def get_required_version_ranges(self):
+        """
+        Determines the ranges that a version has to exist within
+        """
+        # List of all allowed ranges
+        ranges = []
+
+        # Keep track of the minimum required spec
+        min_spec = None
+
+        # Keep track of the maximum required spec
+        max_spec = None
+
+        # Loop over all the required specs and calculate the ranges
+        for spec in self.required_version_specs:
+            comparison = spec[0]
+            version = spec[1]
+
+            # Check if this should be the max
+            if comparison in ['<=', '<']:
+                max_spec = spec
+
+            # Check if this should be the min value
+            elif comparison in ['>=', '>']:
+                min_spec = spec
+
+            # Check if we have both a min and a max spec if so push it onto the ranges and reset
+            if min_spec and max_spec:
+                ranges.append((min_spec, max_spec))
+                min_spec = None
+                max_spec = None
+
+        # Add the last range if we need to
+        if min_spec or max_spec:
+            ranges.append((min_spec, max_spec))
+
+        # Return the ranges
+        return ranges
+
+    def get_required_version_exacts(self):
+        """
+        Returns a list of versions that must be exact
+        """
+        # List of exact versions
+        exacts = []
+
+        # Loop over all the required specs get the exacts
+        for spec in self.required_version_specs:
+            comparison = spec[0]
+
+            # Check if the comparison is exact
+            if comparison == '==':
+                exacts.append(spec)
+
+        # Return the exact versions
+        return exacts
+
+    def get_required_version_excludes(self):
+        """
+        Returns a list of versions that we need to exclude
+        """
+        # List of excluded versions
+        excluded = []
+
+        # Loop over all the required specs get the exacts
+        for spec in self.required_version_specs:
+            comparison = spec[0]
+
+            # Check if the comparison is exact
+            if comparison == '!=':
+                excluded.append(spec)
+
+        # Return the excluded version specs
+        return excluded
 
 
 class Checker(object):
@@ -62,7 +240,7 @@ class Checker(object):
         requirement_versions = self.get_requirement_versions()
         installed_versions = self.get_installed_versions()
 
-        # Gets aroung pep8 complaining about unused import
+        # Gets around pep8 complaining about unused import
         assert parse_version
 
         # Find any requirement conflicts
@@ -74,23 +252,18 @@ class Checker(object):
 
             # Get the installed version
             installed_version = installed_versions[project_name]
-            for required_project_name, specs in requirements.items():
-                for spec in specs:
-                    comparison = spec[0]
-                    version = spec[1]
-                    comparison_string = 'parse_version(installed_version) {0} parse_version(version)'.format(
-                        comparison,
-                    )
 
-                    # Evaluate that the installed version passes the comparison of the required version
-                    if not eval(comparison_string):
-                        conflicts.append(Conflict(**{
-                            'project_name': project_name,
-                            'required_project_name': required_project_name,
-                            'comparison': comparison,
-                            'expected_version': version,
-                            'installed_version': installed_version,
-                        }))
+            # Loop over the required dictionaries and determine if we have any dependency conflicts
+            for required_project_name, specs in requirements.items():
+                # Create a validator
+                validator = Validator(installed_version=installed_version, required_version_specs=specs)
+                if not validator.is_valid():
+                    conflicts.append(Conflict(**{
+                        'project_name': project_name,
+                        'required_project_name': required_project_name,
+                        'installed_version': installed_version,
+                        'specs': specs
+                    }))
 
         # Return the conflicts
         return conflicts
@@ -100,6 +273,7 @@ class Checker(object):
 def main():
     checker = Checker()
     conflicts = checker.get_conflicts()
+    pprint(checker.get_requirement_versions())
     if conflicts:
         print('-' * 50)
         print(' Conflicts Detected')
@@ -108,7 +282,7 @@ def main():
             output_string = (
                 ' - ',
                 '{project_name}({installed_version}) ',
-                '{required_project_name}({comparison}{expected_version})'
+                '{required_project_name}({readable_specs})'
             )
             print(''.join(output_string).format(
                 **conflict.__dict__
--- pipconflictchecker/tests/checker_tests.py
+++ pipconflictchecker/tests/checker_tests.py
@@ -1,7 +1,303 @@
 from mock import patch, Mock
 from pip._vendor.pkg_resources import Distribution, Requirement
 from unittest import TestCase
-from pipconflictchecker.checker import Checker, main
+from pipconflictchecker.checker import Checker, main, Validator
+
+
+class ValidatorTest(TestCase):
+    def test_init(self):
+        installed_version = '1.0'
+        specs = [
+            ('<=', '2.0'),
+            ('>=', '1.0')
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Assert that the properties were set
+        self.assertTrue(validator.installed_version, installed_version)
+        self.assertTrue(validator.required_version_specs, specs)
+
+        # Assert that the required version specs were sorted properly
+        self.assertTrue(validator.required_version_specs[0], specs[1])
+        self.assertTrue(validator.required_version_specs[1], specs[0])
+
+    def test_ranges_no_max(self):
+        installed_version = '1.0'
+        specs = [
+            ('>=', '1.0')
+        ]
+        validator = Validator(installed_version, specs)
+        ranges = validator.get_required_version_ranges()
+
+        # Validate there is only one range
+        self.assertEqual(len(ranges), 1)
+
+        # Validate that there is no max spec
+        self.assertIsNone(ranges[0][1])
+
+    def test_ranges_no_min(self):
+        installed_version = '0.1'
+        specs = [
+            ('<=', '1.0')
+        ]
+        validator = Validator(installed_version, specs)
+        ranges = validator.get_required_version_ranges()
+
+        # Validate there is only one range
+        self.assertEqual(len(ranges), 1)
+
+        # Validate that there is no max spec
+        self.assertIsNone(ranges[0][0])
+
+    def test_ranges_multiple_min(self):
+        installed_version = '0.1'
+        specs = [
+            ('<=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        ranges = validator.get_required_version_ranges()
+
+        # Validate there is only one range
+        self.assertEqual(len(ranges), 1)
+
+        # Validate that there is no max spec
+        self.assertIsNone(ranges[0][0])
+
+    def test_ranges_min_and_max(self):
+        installed_version = '1.0'
+        specs = [
+            ('<=', '2.0'),
+            ('>=', '1.0')
+        ]
+        validator = Validator(installed_version, specs)
+        ranges = validator.get_required_version_ranges()
+
+        # Validate there is only one range
+        self.assertEqual(len(ranges), 1)
+
+        # Validate that the min and max are correct
+        self.assertEqual(ranges[0][0], specs[1])
+        self.assertEqual(ranges[0][1], specs[0])
+
+    def test_ranges_multiple_no_max(self):
+        installed_version = '1.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('>=', '3.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        ranges = validator.get_required_version_ranges()
+
+        # Validate the number of ranges
+        self.assertEqual(len(ranges), 2)
+
+    def test_exacts(self):
+        installed_version = '1.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('>=', '3.0'),
+            ('==', '2.5'),
+        ]
+        validator = Validator(installed_version, specs)
+        exacts = validator.get_required_version_exacts()
+
+        # Validate the number of exacts
+        self.assertEqual(len(exacts), 1)
+
+    def test_excludes(self):
+        installed_version = '1.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('>=', '3.0'),
+            ('!=', '3.5'),
+        ]
+        validator = Validator(installed_version, specs)
+        excludes = validator.get_required_version_excludes()
+
+        # Validate the number of exacts
+        self.assertEqual(len(excludes), 1)
+
+    def test_in_ranges_true_no_max_non_edge(self):
+        installed_version = '2.0'
+        specs = [
+            ('>=', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are in the range
+        self.assertTrue(validator.in_ranges())
+
+    def test_in_ranges_false_no_max_non_edge(self):
+        installed_version = '0.9'
+        specs = [
+            ('>=', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are not in the range
+        self.assertFalse(validator.in_ranges())
+
+    def test_in_ranges_true_no_max_edge(self):
+        installed_version = '1.0'
+        specs = [
+            ('>=', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are in the range
+        self.assertTrue(validator.in_ranges())
+
+    def test_in_ranges_false_no_max_edge(self):
+        installed_version = '1.0'
+        specs = [
+            ('>', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are not in the range
+        self.assertFalse(validator.in_ranges())
+
+    def test_in_ranges_true_min_and_max_non_edge(self):
+        installed_version = '1.5'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are in the range
+        self.assertTrue(validator.in_ranges())
+
+    def test_in_ranges_false_min_and_max_non_edge(self):
+        installed_version = '3.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are not in the range
+        self.assertFalse(validator.in_ranges())
+
+    def test_in_ranges_true_min_and_max_edge(self):
+        installed_version = '2.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are in the range
+        self.assertTrue(validator.in_ranges())
+
+    def test_in_ranges_false_min_and_max_edge(self):
+        installed_version = '2.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+
+        # Validate we are not in the range
+        self.assertFalse(validator.in_ranges())
+
+    def test_in_exacts_true(self):
+        installed_version = '1.0'
+        specs = [
+            ('==', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertTrue(validator.in_exacts())
+
+    def test_in_exacts_false(self):
+        installed_version = '2.0'
+        specs = [
+            ('==', '1.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertFalse(validator.in_exacts())
+
+    def test_in_excludes_true(self):
+        installed_version = '1.1'
+        specs = [
+            ('>=', '1.0'),
+            ('!=', '1.1'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertTrue(validator.in_excludes())
+
+    def test_in_excludes_false(self):
+        installed_version = '1.2'
+        specs = [
+            ('>=', '1.0'),
+            ('!=', '1.1'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertFalse(validator.in_excludes())
+
+    def test_is_valid_true_one_range(self):
+        installed_version = '2.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertTrue(validator.is_valid())
+
+    def test_is_valid_false_one_range(self):
+        installed_version = '3.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertFalse(validator.is_valid())
+
+    def test_is_valid_true_multi_range(self):
+        installed_version = '2.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('>=', '3.0'),
+            ('<=', '4.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertTrue(validator.is_valid())
+
+    def test_is_valid_false_multi_range(self):
+        installed_version = '2.1'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('>=', '3.0'),
+            ('<=', '4.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertFalse(validator.is_valid())
+
+    def test_is_valid_true_in_exacts_not_in_ranges(self):
+        installed_version = '3.0'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('==', '3.0'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertTrue(validator.is_valid())
+
+    def test_is_valid_false_in_excludes_in_ranges(self):
+        installed_version = '1.5'
+        specs = [
+            ('>=', '1.0'),
+            ('<=', '2.0'),
+            ('!=', '1.5'),
+        ]
+        validator = Validator(installed_version, specs)
+        self.assertFalse(validator.is_valid())
 
 
 class CheckerTest(TestCase):
